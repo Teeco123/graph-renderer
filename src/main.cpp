82c3703198fb2b7745b1bc29fc24d98378a1e6dd
Kacper Gajko <kacper.gajko1@icloud.com>
2025-03-09 00:36:13 +0100

    added slider for radius and count & comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,50 @@
 #include "utils.h"
 
 int main() {
+  //------------------------------------------------------------------------------------
+  // Resolution settings
   const int screenHeight = 800;
-  const int screenWidth = 1000;
+  const int screenWidth = 1200;
   Vector2 resolution = {800, 800};
 
-  // All raylib settings
+  //------------------------------------------------------------------------------------
+  // Settings variables
+  int pointsCount = 202;
+  float pointsCountFloat = 202;
+  float radius = 80; // Influence radius
+
+  //------------------------------------------------------------------------------------
+  // Raylib settings
   InitWindow(screenWidth, screenHeight, "Graph Renderer");
   SetTargetFPS(120);
   GuiLoadStyleCyber();
 
-  Shader shader = LoadShader(0, TextFormat("src/shaders/test.fragment", 330));
-
+  //------------------------------------------------------------------------------------
   // Loading points
-  const int pointsCount = 202;
   Point point[pointsCount];
   InitRandomPoints(point, pointsCount);
 
-  float radius = 80; // Influence radius
-
+  //------------------------------------------------------------------------------------
+  // Setting up shader
+  Shader shader = LoadShader(0, TextFormat("src/shaders/test.fragment", 330));
   int pointsLoc = GetShaderLocation(shader, "points");
   int numPointsLoc = GetShaderLocation(shader, "numPoints");
   int radiusLoc = GetShaderLocation(shader, "radius");
   int resolutionLoc = GetShaderLocation(shader, "resolution");
 
-  SetShaderValue(shader, numPointsLoc, &pointsCount, SHADER_UNIFORM_INT);
-  SetShaderValue(shader, radiusLoc, &radius, SHADER_UNIFORM_FLOAT);
-  SetShaderValue(shader, resolutionLoc, &resolution, SHADER_UNIFORM_VEC2);
-  SetShaderValueV(shader, pointsLoc, point, SHADER_UNIFORM_VEC2, pointsCount);
+  while (!WindowShouldClose()) { // Game loop
 
-  while (!WindowShouldClose()) {
+    //------------------------------------------------------------------------------------
+    // Passing values to shader
+    SetShaderValue(shader, numPointsLoc, &pointsCount, SHADER_UNIFORM_INT);
+    SetShaderValue(shader, radiusLoc, &radius, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(shader, resolutionLoc, &resolution, SHADER_UNIFORM_VEC2);
+    SetShaderValueV(shader, pointsLoc, point, SHADER_UNIFORM_VEC2, pointsCount);
 
-    BeginDrawing();
-    ClearBackground(GRAY);
+    BeginDrawing(); // Begin draw
 
+    //------------------------------------------------------------------------------------
+    // Main simulation window
     BeginShaderMode(shader);
     DrawRectangle(0, 0, 800, 800, WHITE);
     EndShaderMode();
@@ -48,6 +59,18 @@ int main() {
     for (int i = 0; i < pointsCount; i++) {
       DrawCircleV(point[i].position, 5, WHITE);
     }
+
+    //------------------------------------------------------------------------------------
+    // Settings window
+    GuiWindowBox((Rectangle){800, 0, 400, 800}, TextFormat("Settings"));
+
+    GuiSlider((Rectangle){802, 25, 250, 20}, NULL,
+              TextFormat("Radius: %0.2f", radius), &radius, 0, 300);
+
+    pointsCount = static_cast<int>(pointsCountFloat);
+    GuiSlider((Rectangle){802, 45, 250, 20}, NULL,
+              TextFormat("Points count: %0.2f", pointsCountFloat),
+              &pointsCountFloat, 0, 202);
 
     EndDrawing();
   }
